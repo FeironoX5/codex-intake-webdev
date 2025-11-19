fcbcd5c5e56eab1a8f19a70b4dd665117aa71ef5
Gleb Kiva <feironox5@gmail.com>
2025-11-19 17:24:16 +0300

    chore(front): app config moved

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,5 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
 
-bootstrapApplication(App, appConfig).catch((err) => console.error(err));
+bootstrapApplication(App, {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+  ],
+}).catch((err) => console.error(err));
